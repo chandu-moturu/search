8f53d7c5959d0d972db65f14ef0093a0cd4d050f
mr-innocent08 <89263616+mr-innocent08@users.noreply.github.com>
2023-04-11 21:06:49 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState ,useMemo, useRef} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [items, setItems] = useState([])
+  const [query,setQuery] = useState("")
+  const inputRef = useRef()
+  
+  //Dom manipulation is done whenever the query changes items also displayed accordingly
+
+  const filteredItems = useMemo(()=>{
+    return items.filter(item=>{
+      return item.toLowerCase().includes(query.toLowerCase())
+    })
+  },[items,query])
+
+
+  //handles the submission
+  function onSubmit(e){
+    e.preventDefault()
+
+    const value = inputRef.current.value
+    if(value === "") return
+    setItems(prev => {
+      return [...prev,value]
+    })
+
+    inputRef.current.value = " "
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+
+      
+
+      <div className='form'>
+          <form onSubmit={onSubmit}>
+            <h3>Add Items</h3>
+            <input type="text" ref={inputRef}/>
+            <br/>
+
+            <button type="submit">Add</button>
+          </form>
+      </div>
+
+      <div className='search'>
+          <h3>Search Items</h3>
+          <input value={query} onChange={e=> setQuery(e.target.value)} type="search" />
+      </div>
+
+      <div className='item'>
+          <h3>Items:</h3>
+          
+          {
+            (filteredItems.length!==0)?
+            filteredItems.map(item=>(
+              <div className='items'>{item}</div>
+            )):
+            "0 Items.."
+          }
+          
+
+      </div>
+      
     </div>
+      
   );
 }
+      
 
 export default App;
